a402305db6e924e9820cd3480be9449034dea146
Luca Grillotti <luca.grillotti@sony.com>
2023-12-11 11:20:14 +0900

    loading appropriate data for new robot control

--- diffusion_policy/dataset/kitchen_mjl_lowdim_dataset.py
+++ diffusion_policy/dataset/kitchen_mjl_lowdim_dataset.py
@@ -28,43 +28,65 @@ class KitchenCustomDataset(BaseLowdimDataset):
         if not abs_action:
             raise NotImplementedError()
 
-        robot_pos_noise_amp = np.array([0.1   , 0.1   , 0.1   , 0.1   , 0.1   , 0.1   , 0.1   , 0.1   ,
-            0.1   , ], dtype=np.float32)
+        robot_pos_noise_amp = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], dtype=np.float32)
         rng = np.random.default_rng(seed=seed)
 
-        obs_size = 9
+        obs_size = 7
 
         data_directory = pathlib.Path(dataset_dir)
-        path_qpos = data_directory / "qpos.npy"
-        path_qvel = data_directory / "qvel.npy"
-        path_times = data_directory / "times.npy"
 
         self.replay_buffer = ReplayBuffer.create_empty_numpy()
-        for i in range(15):
-            try:
-                qpos = np.load(path_qpos).astype(np.float32)
-                times_array = np.load(path_times).astype(np.float32)
-                times_array = times_array - times_array[0]
-                qvel = np.load(path_qvel).astype(np.float32)
 
-                obs = qpos[:, :obs_size]
+        DQ_DATA_PATH = "dq_with_time.npy"
+        OBS_DATA_PATH = "obs_with_time.npy"
+
+        for i, np_path in enumerate(tqdm(list(data_directory.glob(f'*/{DQ_DATA_PATH}')))):
+            try:
+                dq_with_time = np.load(np_path.absolute)
+                dq = dq_with_time[:, 1:]
+                _res_folder = np_path.parents[0]
+                obs_path = _res_folder / OBS_DATA_PATH
+                times_to_evaluate = dq_with_time[:, 0].ravel()
+                obs = self.load_obs(obs_path, times_to_evaluate)
+
                 if robot_noise_ratio > 0:
                     # add observation noise to match real robot
                     noise = robot_noise_ratio * robot_pos_noise_amp * rng.uniform(
                         low=-1., high=1., size=(obs.shape[0], obs_size))
                     obs[:, :obs_size] += noise
 
-                    TIME_NOISE = 0.1
-                    noise_time = TIME_NOISE * rng.uniform(low=-1, high=1, size=times_array.shape)
-                    times_array += noise_time
                 episode = {
-                    'obs': np.hstack([qpos, times_array.reshape(-1, 1)]),
-                    'action': obs,
+                    'obs': obs.astype(np.float32),
+                    'action': dq.astype(np.float32)
                 }
                 self.replay_buffer.add_episode(episode)
             except Exception as e:
                 print(i, e)
-                # print(e)
+        # for i in range(15):
+        #     try:
+        #         qpos = np.load(path_qpos).astype(np.float32)
+        #         times_array = np.load(path_times).astype(np.float32)
+        #         times_array = times_array - times_array[0]
+        #         qvel = np.load(path_qvel).astype(np.float32)
+        #
+        #         obs = qpos[:, :obs_size]
+        #         if robot_noise_ratio > 0:
+        #             # add observation noise to match real robot
+        #             noise = robot_noise_ratio * robot_pos_noise_amp * rng.uniform(
+        #                 low=-1., high=1., size=(obs.shape[0], obs_size))
+        #             obs[:, :obs_size] += noise
+        #
+        #             TIME_NOISE = 0.1
+        #             noise_time = TIME_NOISE * rng.uniform(low=-1, high=1, size=times_array.shape)
+        #             times_array += noise_time
+        #         episode = {
+        #             'obs': np.hstack([qpos, times_array.reshape(-1, 1)]),
+        #             'action': obs,
+        #         }
+        #         self.replay_buffer.add_episode(episode)
+        #     except Exception as e:
+        #         print(i, e)
+        #         # print(e)
 
         val_mask = get_val_mask(
             n_episodes=self.replay_buffer.n_episodes,
@@ -82,6 +104,25 @@ class KitchenCustomDataset(BaseLowdimDataset):
         self.horizon = horizon
         self.pad_before = pad_before
         self.pad_after = pad_after
+
+    def load_obs(self, obs_path: pathlib.Path, times_to_evaluate: np.ndarray):
+        obs_with_times = np.load(obs_path)
+        times = obs_with_times[:, 0].ravel()
+        obs = obs_with_times[:, 1:]
+        all_data = []
+        for col in range(obs.shape[1]):
+            _col_data = np.interp(
+                x=times_to_evaluate,
+                xp=times,
+                fp=obs[:, col].ravel(),
+            )
+            all_data.append(_col_data)
+        array_all_data = np.hstack(
+            [_col_data.reshape(-1, 1) for _col_data in all_data]
+        )
+        return array_all_data
+
+
 
     def get_validation_dataset(self):
         val_set = copy.copy(self)
