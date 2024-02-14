0b4c8293d19f396bd7c17699b58645b9fb3cc22b
Luca Grillotti <luca.grillotti@sony.com>
2024-02-14 15:24:35 +0900

    print tree all files to debug everything

--- train.py
+++ train.py
@@ -28,11 +28,22 @@ OmegaConf.register_new_resolver("eval", eval, replace=True)
 def main(cfg: OmegaConf):
     # resolve immediately so all the ${now:} resolvers
     # will use the same time.
+    list_files(".")
+
     OmegaConf.resolve(cfg)
 
     cls = hydra.utils.get_class(cfg._target_)
     workspace: BaseWorkspace = cls(cfg)
     workspace.run()
+
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
 
 if __name__ == "__main__":
     sys.argv = [sys.argv[0]]
